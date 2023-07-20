28e83562b793579a497169489573c523eabb60bf
Zach Bloss <zacharybloss@gmail.com>
2023-07-20 00:21:56 -0400

    add local imports
    add swish gate

---
+++ retentive_network/layers/__init__.py
@@ -0,0 +1,4 @@
+from .feed_forward import FeedForward
+from .multi_scale_retention import MultiScaleRetention
+from .retention import Retention
+from .swish_gate import SwishGate
--- retentive_network/layers/retention.py
+++ retentive_network/layers/retention.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
 
 class Retention(nn.Module):
     def __init__(
