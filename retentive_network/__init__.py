e68dda9527fc1254397ba07bfa04bc08d9c44c00
Zach Bloss <zacharybloss@gmail.com>
2023-07-21 23:59:05 -0400

    remove __version__

--- retentive_network/__init__.py
+++ retentive_network/__init__.py
@@ -1,9 +1,2 @@
 from . import layers, models
 from .exceptions import *
-
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
-__version__ = importlib_metadata.version(__name__)
